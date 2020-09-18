b59e949fa9e14ae2fbda4ca51c7b5755807e3b75
Adam-Young <awy6448@gmail.com>
2020-09-17 21:28:59 -0400

    used the Linked list for stacks and queues

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../singly_linked_list/')
+from singly_linked_list import LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,18 +16,18 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def push(self, value):
         self.size += 1
-        return self.storage.insert(0, value)
+        return self.storage.add_to_tail(value)
 
     def pop(self):
         if (self.size == 0):
           return None
         else:
           self.size -= 1
-          return self.storage.pop(0)
+          return self.storage.remove_tail()
